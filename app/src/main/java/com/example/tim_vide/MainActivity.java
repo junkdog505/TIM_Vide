662b3e9cb06a6498568dc8ffd548be156312584c
junkdog505 <junkdog505@gmail.com>
2023-04-09 22:48:31 -0500

    Correción de la correción del teclado

--- app/src/main/java/com/example/tim_vide/MainActivity.java
+++ app/src/main/java/com/example/tim_vide/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.tim_vide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teclado);
     }
