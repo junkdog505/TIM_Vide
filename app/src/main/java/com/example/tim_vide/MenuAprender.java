373bf19c07c6e6560c174e396b9d779992fdfe94
junkdog <junkdog505@gmail.com>
2023-06-24 04:05:01 -0500

    Todos los teclados funcional y Guía de números y letras puestas

--- app/src/main/java/com/example/tim_vide/MenuAprender.java
+++ app/src/main/java/com/example/tim_vide/MenuAprender.java
@@ -6,30 +6,31 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class Home extends AppCompatActivity {
+public class MenuAprender extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        ImageButton btn1 = findViewById(R.id.btnAprender);
-        ImageButton btn2 = findViewById(R.id.btnPracticar);
+        setContentView(R.layout.activity_menu_aprender);
+        ImageButton btn1 = findViewById(R.id.btnLetras);
+        ImageButton btn2 = findViewById(R.id.btnNumeros);
+        ImageButton btn3 = findViewById(R.id.btnSignos);
+
 
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(Home.this, MenuAprender.class);
+                Intent intent = new Intent(MenuAprender.this,  AprenderView.class);
                 startActivity(intent);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Home.this, Practicar.class);
+                Intent intent = new Intent(MenuAprender.this, AprenderNums.class);
                 startActivity(intent);
             }
         });
